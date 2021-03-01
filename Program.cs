32e21d1c02381658adbf0d0c94d25502a42be4b9
Carlos <cjrfls@gmail.com>
2021-03-01 09:58:11 -0700

    Se le genera una carta aleatoria al jugador

--- Program.cs
+++ Program.cs
@@ -17,6 +17,25 @@ namespace BlackJack
             Console.WriteLine("1) Si desea obtener una carta.");
             Console.WriteLine("2) Si desea retirarse.");
             int eleccion = Convert.ToInt32(Console.ReadLine());
+            if (eleccion == 1)
+                {
+                    // n = n+1;
+                /// <summary>
+                /// Obtener carta aleatorias
+                /// </summary>
+                /// <returns>1 o 2</returns>
+                
+                    Random rnd = new Random();
+                    int O_Cartas = rnd.Next(1, 12);
+                    
+                    Console.WriteLine("Tu carta vale: " + O_Cartas);
+            
+
+                    
+                    }
+                    
+
+                }
 
         }
         
@@ -31,4 +50,3 @@ namespace BlackJack
 
 
     }
-}
